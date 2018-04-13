d93359e000b53e8313d6fd1b6dce38137d5a01ec
KamalMagdy <kamal.m2331@yahoo.com>
2018-04-13 22:23:50 +0200

    index page layout + date modified

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,17 +1,31 @@
 <html>
 <head>
 <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
 <!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 </head>
 <body>
 
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">ITI Blog</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="/posts">All Posts</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<br>
 @yield('content')
 
 </body>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,19 +2,43 @@
 
 
 @section('content')
+<center>
+ <a class="btn btn-success" href="posts/create">Create Post</a>
+</center>
+<br>
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#Pagination Bonus</th>
+      <th scope="col">Title</th>
+      <th scope="col">Posted By</th>
+      <th scope="col">Created At</th>
+      <th scope="col">Actions</th>
 
-<h1>Posts Index</h1>
-<ul>
-
+    </tr>
+  </thead>
+ 
 @foreach ($posts as $post)
 
 
-<li>{{ $post->title }}  And The Creator is ({{$post->user->name}})</li>
+<!-- <li>{{ $post->title }}  And The Creator is ({{$post->user->name}})</li> -->
 
+ <tbody>
+    <tr>
+      <th scope="row">{{ $post->id }}</th>
+      <td>{{ $post->title }}</td>
+      <td>{{$post->user->name}}</td>
+      <td>{{$post->created_at->todatestring()}}</td>
+      <td>
+      <a class="btn btn-info" href="posts/create">view</a>
+      <a class="btn btn-primary" href="posts/create">edit</a>
+      <a class="btn btn-danger" href="posts/create">delete</a>
+      </td>
+
+    </tr>
+  </tbody>
 
 @endforeach
-
-
-</ul>
+</table>
 
 @endsection
